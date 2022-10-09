13087d2f763f315a85d4cbadee97dc36817b317e
Aryaveer Chaudhary <101991741+DicE-007@users.noreply.github.com>
2022-10-09 23:02:28 +0530

    Update frogjump.cpp

--- frogjump.cpp
+++ frogjump.cpp
@@ -8,42 +8,55 @@ void tester() {
 #endif
 }
 //memoization
-int solve(vector<int>arr, int n, vector<int>&dp) {
-	int left = INT_MAX;
-	int right = INT_MAX;
-	if (n == 0)return 0;
-	if (dp[n] != -1)return dp[n];
-	left = solve(arr, n - 1, dp) + abs(arr[n] - arr[n - 1]);
-	if (n > 1) {
-		right = solve(arr, n - 2, dp) + abs(arr[n] - arr[n - 2]);
+int helper(vector<int>&arr, vector<int>&dp, int n) {
+	if (n == 0) {
+		return 0;
 	}
-	return dp[n] = min(right, left);
+	if (dp[n] != -1)return dp[n];
+	int j1 = helper(arr, dp, n - 1) + abs(arr[n] - arr[n - 1]);
+	int j2 = INT_MAX;
+	if (n > 1)
+		j2 = helper(arr, dp, n - 2) + abs(arr[n] - arr[n - 2]);
+	return dp[n] = min(j1, j2);
 }
-
-
+//tabulation
+int helper2(vector<int>&arr, vector<int>&dp, int n) {
+	dp[0] = 0;
+	for (int i = 1; i < n; i++) {
+		int j1 = dp[i - 1] + abs(arr[i] - arr[i - 1]);
+		int j2 = INT_MAX;
+		if (i > 1) {
+			j2 = dp[n - 2] + abs(arr[i] - arr[i - 2]);
+		}
+		dp[i] = min(j1, j2);
+	}
+	return dp[n - 1];
+}
+//space
+int helper3(vector<int>&arr, int n) {
+	int prev = 0;
+	int prev2 = 0;
+	for (int i = 1; i < n; i++) {
+		int j1 = prev + abs(arr[i] - arr[i - 1]);
+		int j2 = INT_MAX;
+		if (i > 1)
+			j2 = prev2 + abs(arr[i] + arr[i - 2]);
+		int cur = min(j1, j2);
+		prev2 = prev;
+		prev = cur;
+	}
+	return prev;
+}
 int main() {
 	tester();
-
-	vector<int>arr = {10, 20, 30, 10, 50, 10, 20, 30, 55, 56};
-	int n = arr.size();
-	vector<int>arr2 = {30, 10, 60, 10, 60, 50, 20, 50, 34, 12, 54, 76, 78};
-	int m = arr2.size();
-	vector<int>dp1(n + 1, -1);
-	vector<int>dp2(m + 1, -1);
-	cout << solve(arr, n - 1 , dp1) << endl;
-	cout << solve(arr2, m - 1 , dp2) << endl;
-	//tabulation
-	vector<int>arr3 = {10, 20, 30, 10, 50, 10, 20, 30, 55, 56};
-	int o = arr.size();
-	vector<int>dynap( o + 1, -1);
-	dynap[0] = 0;
-	for (int i = 1; i < o; i++) {
-		int firstss = dynap[i - 1] + abs(arr3[i] - arr3[i - 1]);
-		int secondss = INT_MAX;
-		if (i > 1)
-			secondss = dynap[i - 2] + abs(arr3[i] - arr3[i - 2]);
-		dynap[i] = min(firstss, secondss);
-
+	int n; cin >> n;
+	vector<int>stair(n);
+	vector<int>dp(n + 1, -1);
+	vector<int>dp2(n + 1, -1);
+	for (int i = 0; i < n; i++) {
+		cin >> stair[i];
 	}
-	cout << dynap[o - 1];
+	cout << helper(stair, dp, n) << endl;
+	cout << helper2(stair, dp2, n ) << endl;
+	cout << helper3(stair, n) << endl;
 }
