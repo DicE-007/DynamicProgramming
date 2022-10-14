3212508c476f5abca38b01f84c3bcc63736fb0d9
Aryaveer Chaudhary <101991741+DicE-007@users.noreply.github.com>
2022-10-14 21:19:49 +0530

    Update griduniuqpaths.cpp

--- griduniuqpaths.cpp
+++ griduniuqpaths.cpp
@@ -1,3 +1,4 @@
+//grid total paths
 class Solution {
 public:
     int solve(int i,int j,vector<vector<int>>&grid){
@@ -66,4 +67,40 @@ public:
         // return solveTabu(m,n,grid);
         return solveTabuSo(m,n,grid);
     }
+    //grid unique paths
+    class Solution {
+public:
+    int helper(vector<vector<int>>grid,int i,int j,vector<vector<int>>&dp){
+        if(i==0 && j==0)return grid[0][0];
+        if(i<0 || j<0) return 1e9;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int left = grid[i][j]+helper(grid,i-1,j,dp);
+        int up = grid[i][j]+helper(grid,i,j-1,dp);
+        return dp[i][j] = min(left,up);
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n =grid[0].size();
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        //return helper(grid,m-1,n-1,dp);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) dp[i][j]=grid[i][j];
+                else{
+                    int left = grid[i][j],up=grid[i][j];
+                    if(i>0){
+                        left+=dp[i-1][j];
+                    }
+                    else left+=1e9;//if there is no way to go left then qwe will make it infinity so we can get minimum
+                    if(j>0){
+                        up+=dp[i][j-1];
+                    }
+                    else up+=1e9;
+                    dp[i][j]=min(up,left);
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
 };
